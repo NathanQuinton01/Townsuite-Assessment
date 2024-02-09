820c30b2c6884f44cc5e06eae1faa788b3652c5a
NathanQuintonMun <159549857+NathanQuinton01@users.noreply.github.com>
2024-02-09 11:39:43 -0330

    Update GuessingGame.py

--- Questions/GuessingGame.py
+++ Questions/GuessingGame.py
@@ -1,8 +1,13 @@
 correct = False
 
 while (not correct):
-    guess = int(input("Guess a number between 1 and 100.\n"))
+    guess = input("Guess a number between 1 and 100.\n")
 
+    if not guess.isnumeric(): # Prevent guesses which are not numbers
+        print("Please guess a number!")
+        continue
+
+    guess = int(guess)
     if guess < 1 or guess > 100: # Prevent guesses out of bounds
         print("Guess is out of range!")
         continue
